20f0e07c96d99756a651949adfe5852ebf5f9919
Grigory <zakirov.gs@yandex.ru>
2023-05-18 20:47:02 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,103 @@
+"use strict"
+
+let center = [48.507083521407516, 135.11596420076776];
+
+function init() {
+    let map = new ymaps.Map(`map`, {
+        center: center,
+        zoom: 17
+
+    });
+
+    let placemark = new ymaps.Placemark(center, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/generic/location-pin.png',
+        iconImageSize: [40, 40],
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем поиск
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+
+    map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                // formPreview.innerHTML = '';
+                form.reset();
+                form.classList.remove('_sending');
+            } else {
+                alert("Ошибка!");
+                form.classList.remove('_sending');
+            }
+
+        } else {
+            alert('Заполните обязательные поля')
+        }
+
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
